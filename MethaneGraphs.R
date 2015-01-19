27a2021d700d4a165f0783ae5cae3431c5a9ee36
unknown <slpitz@gmail.com>
2015-01-19 15:44:49 -0500

    Added early flux data and sap flux data as well

--- MethaneGraphs.R
+++ MethaneGraphs.R
@@ -5,10 +5,11 @@ library(ggplot2)
 library(grid)
 library(gridExtra)
 library(plyr)
+setwd("C:/Users/Pitz/Desktop/R-Graphs")
 
 nmmaps<-read.csv("automated.csv", as.is=T) #load files
 head(nmmaps) #show beginning of file
-#g<-ggplot(nmmaps, aes(Julian, CH4))+geom_point(color="firebrick")
+#g<- ggplot(nmmaps, aes(Julian, CH4))+geom_point(color="firebrick")
 
 #CH4 only graph
 autoch4<-nmmaps[nmmaps$CH4flag=="Y",] # drop rows where flux R2 <0.80
@@ -184,4 +185,194 @@ supup<-ggplot(uptotal, aes(mean.y, mean.x, color=factor(Type)))+geom_point(aes(s
 supup<-supup+labs(x="Date", y=expression(paste("Gas Fluxes (", mu ~ g/m^2 , "h )")), title="2014 - Stem Gas Fluxes - Upland All RSQ")
 supup<-supup+theme(legend.title = element_text(colour="chocolate", size=16, face="bold"))+scale_color_discrete(name="Flux Type")
 supup
+
+#plot sapflow data
+sap<-read.csv("SapFlow_trees7085_7098.csv", as.is=T) #load files
+head(sap) #show beginning of file
+saplate<-sap[sap$jday>209.7,]
+saplate<-saplate[saplate$jday<213.0,]
+head(saplate)
+
+#rearrange data so all fluxes in one column
+#facet_grid separates data into tiles
+beech<-autofacet[autofacet$Chamber=="7098-75",]
+#beech<-beech[beech$Gas=="CH4",]
+gf<-ggplot(beech, aes(Julian, Flux, color=factor(Chamber)))+geom_point()
+#gf+theme(legend.title = element_text(colour="chocolate", size=16, face="bold"))+
+#scale_color_discrete(name="This color is\ncalled chocolate!?")
+gf<-gf+labs(x="Julian Date", y=expression(paste("Gas Fluxes (", mu ~ g/m^2 , "h )")), title="Stem Gas Fluxes")
+gf + facet_grid(Gas ~ ., scales="free_y") #scales="free_y" allows each graph's y-axis to scale independently
+
+beechsap<-ggplot(saplate, aes(jday, Fd_7098))+geom_point()
+#c<-c+ggtitle('Stem Respiration')
+beechsap<-beechsap+labs(x="Julian Date", y=expression(paste("Stem Respiration ( CO2", mu ~ g/m^2 , "h )")), title="Stem Respiration")
+beechsap
+
+ 
+
+#average data to the hour
+df <- data.frame(Julian = numeric(), Type = character(), Value = numeric(), stringsAsFactors = FALSE)
+
+x<-209.0+1050/1440
+i <-0;
+while(x < 212.75) {y <- x+1/24;
+                time <- 0;
+                print(x);
+                bwt<-beech[beech$Julian>x,];
+                bwt<-bwt[bwt$Julian<y,];
+                bwt<-bwt[bwt$Gas=="CH4",];
+                time<-(x+y)/2;
+                AveFlux<-mean(bwt$Flux);
+                df <- rbind(df, data.frame(Julian = time, Type = "CH4", Value=AveFlux));
+                x <- x+1/24;
+                i<-i+1;
+}
+
+x<-209.0+1050/1440
+
+while(x < 212.75) {y <- x+1/24;
+                   time <- 0;
+                   print(x);
+                   bwt<-sap[sap$jday>x,];
+                   bwt<-bwt[bwt$jday<y,];
+                   time<-(x+y)/2;
+                   AveFlux<-mean(bwt$Fd_7098);
+                   df <- rbind(df, data.frame(Julian = time, Type = "SAP", Value=AveFlux));
+                   x <- x+1/24;
+}
+
+x<-209.0+1050/1440
+beechCO2<-beech[beech$Gas=="CO2",]
+while(x < 212.75) {y <- x+1/24;
+                   time <- 0;
+                   print(x);
+                   bwt<-beechCO2[beechCO2$Julian>x,];
+                   bwt<-bwt[bwt$Julian<y,];
+                   time<-(x+y)/2;
+                   AveFlux<-mean(bwt$Flux);
+                   df <- rbind(df, data.frame(Julian = time, Type = "CO2", Value=AveFlux));
+                   x <- x+1/24;
+}
+df
+
+#rearrange data so all fluxes in one column
+#facet_grid separates data into tiles
+beechtri<-ggplot(df, aes(Julian, Value, color=factor(Type)))+geom_point()
+beechtri<-beechtri+labs(x="Julian Date", y=expression(paste("Gas Fluxes (", mu ~ g/m^2 , "h )")), title="Beech Stem Gas Fluxes")
+beechtri + facet_grid(Type ~ ., scales="free_y") #scales="free_y" allows each graph's y-axis to scale independently
+
+#average data to the hour
+df1 <- data.frame(Julian = numeric(), Type = character(), Value = numeric(), stringsAsFactors = FALSE)
+
+x<-209.0+1050/1440
+i <-0;
+while(x < 212.75) {y <- x+1/24;
+                   time <- 0;
+                   bwt<-beech[beech$Julian>x,];
+                   bwt<-bwt[bwt$Julian<y,];
+                   bwt<-bwt[bwt$Gas=="CH4",];
+                   bwt<-bwt[bwt$CH4R>0.8,];
+                   time<-(x+y)/2;
+                   AveFlux<-mean(bwt$Flux);
+                   df1 <- rbind(df1, data.frame(Julian = time, Type = "CH4", Value=AveFlux));
+                   x <- x+1/24;
+                   i<-i+1;
+}
+
+x<-209.0+1050/1440
+
+while(x < 212.75) {y <- x+1/24;
+                   time <- 0;
+                   bwt<-sap[sap$jday>x,];
+                   bwt<-bwt[bwt$jday<y,];
+                   time<-(x+y)/2;
+                   AveFlux<-mean(bwt$Fd_7098);
+                   df1 <- rbind(df1, data.frame(Julian = time, Type = "SAP", Value=AveFlux));
+                   x <- x+1/24;
+}
+
+x<-209.0+1050/1440
+beechCO2<-beech[beech$Gas=="CO2",]
+while(x < 212.75) {y <- x+1/24;
+                   time <- 0;
+                   print(x);
+                   bwt<-beechCO2[beechCO2$Julian>x,];
+                   bwt<-bwt[bwt$Julian<y,];
+                   bwt<-bwt[bwt$CO2R>0.8,];
+                   time<-(x+y)/2;
+                   AveFlux<-mean(bwt$Flux);
+                   df1 <- rbind(df1, data.frame(Julian = time, Type = "CO2", Value=AveFlux));
+                   x <- x+1/24;
+}
+df
+
+#rearrange data so all fluxes in one column
+#facet_grid separates data into tiles
+beechtrisig<-ggplot(df1, aes(Julian, Value, color=factor(Type)))+geom_point()
+#gf+theme(legend.title = element_text(colour="chocolate", size=16, face="bold"))+
+#scale_color_discrete(name="This color is\ncalled chocolate!?")
+beechtrisig<-beechtrisig+labs(x="Julian Date", y=expression(paste("Gas Fluxes (", mu ~ g/m^2 , "h )")), title="Beech Stem Gas Fluxes")
+beechtrisig + facet_grid(Type ~ ., scales="free_y") #scales="free_y" allows each graph's y-axis to scale independently
+
+full<-read.csv("Faceted.csv", as.is=T) #load files
+head(full) #show beginning of file
+tail(full)
+
+#average data to the hour
+df2 <- data.frame(Julian = numeric(), Type = character(), Value = numeric(), stringsAsFactors = FALSE)
+
+x<-203.0+990/1440
+while(x < 204.73) {y <- x+1/24;
+                   time <- 0;
+                   bwt<-full[full$Julian>x,];
+                   bwt<-bwt[bwt$Julian<y,];
+                   bwt<-bwt[bwt$Type=="CH4",];
+                   bwt<-bwt[bwt$Chamber=="7085-245",];
+                   bwt<-bwt[bwt$CH4R>0.8,];
+                   time<-(x+y)/2;
+                   AveFlux<-mean(bwt$Flux);
+                   df2 <- rbind(df2, data.frame(Julian = time, Type = "CH4", Value=AveFlux));
+                   x <- x+1/24;
+}
+
+x<-203.0+990/1440
+while(x < 204.73) {y <- x+1/24;
+                   time <- 0;
+                   bwt<-sap[sap$jday>x,];
+                   bwt<-bwt[bwt$jday<y,];
+                   time<-(x+y)/2;
+                   AveFlux<-mean(bwt$Fd_7085);
+                   df2 <- rbind(df2, data.frame(Julian = time, Type = "SAP", Value=AveFlux));
+                   x <- x+1/24;
+}
+
+x<-203.0+990/1440
+while(x < 204.73) {y <- x+1/24;
+                   time <- 0;
+                   print(x);
+                   bwt<-full[full$Julian>x,];
+                   bwt<-bwt[bwt$Julian<y,];
+                   bwt<-bwt[bwt$Type=="CO2",];
+                   bwt<-bwt[bwt$Chamber=="7085-245",];
+                   bwt<-bwt[bwt$CO2R>0.8,];
+                   time<-(x+y)/2;
+                   AveFlux<-mean(bwt$Flux);
+                   df2 <- rbind(df2, data.frame(Julian = time, Type = "CO2", Value=AveFlux));
+                   x <- x+1/24;
+}
+
+#facet_grid separates data into tiles
+tpTriSig<-ggplot(df2, aes(Julian, Value, color=factor(Type)))+geom_point()
+tpTriSig<-tpTriSig+labs(x="Julian Date", y=expression(paste("Gas Fluxes (", mu ~ g/m^2 , "h )")), title="Tulip Poplar Stem Gas Fluxes")
+tpTriSig + facet_grid(Type ~ ., scales="free_y") #scales="free_y" allows each graph's y-axis to scale independently
+
+#combine df1 - beech data- with df2 - tp data- into one dataframe
+total <- rbind(df2, df1)
+total$tree_f <- factor(total$Tree, levels=c('Tulip Poplar','Beech'))
+
+#facet_grid separates data into tiles
+AllSap<-ggplot(total, aes(Julian, Value, color=factor(Type)))+geom_point()
+AllSap<-AllSap+labs(x="Julian Date", y=expression(paste("Gas Fluxes (", mu ~ g/m^2 , "h )")), title="Stem Gas Fluxes")
+#AllSap + facet_grid(Type ~ tree_f, scales="free",space="free_x") #scales="free_y" allows each graph's y-axis to scale independently
+AllSap + facet_grid(Type ~ tree_f, scales="free") #scales="free_y" allows each graph's y-axis to scale independently
 
